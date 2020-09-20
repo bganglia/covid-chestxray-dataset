a4476c740bfc50d5f5244bff2a3249ac3dcb559b
bganglia <bganglia892@gmail.com>
2020-09-20 17:11:04 -0400

    Adjust default search term

--- scripts/scraping/run.sh
+++ scripts/scraping/run.sh
@@ -1,2 +1,2 @@
 source scraping_environment/bin/activate
-python combined_interface.py --search "pneumonia" --images new_covid/ --csv new_covid.csv --old ../../metadata.csv --using internal --max 10000000000000000000000 --exclude exclude.txt
+python combined_interface.py --search "covid" --images new_covid/ --csv new_covid.csv --old ../../metadata.csv --using internal --max 10000000000000000000000 --exclude exclude.txt
